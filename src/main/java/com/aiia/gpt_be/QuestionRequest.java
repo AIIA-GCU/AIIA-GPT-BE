f8c28a363514dffbfa8a123e4c65668a22de1b61
han16935 <han1693516@gmail.com>
2024-08-08 14:10:01 +0900

    Refactor name (QuestionForm > QuestionRequest)

---
+++ src/main/java/com/aiia/gpt_be/QuestionRequest.java
@@ -0,0 +1,25 @@
+package com.aiia.gpt_be;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class QuestionRequest {
+
+    @NotBlank(message = "질문을 입력해주세요!")
+    private String question;
+
+    @Builder
+    private QuestionRequest(String question) {
+        this.question = question;
+    }
+
+    public static QuestionRequest of(String question){
+        return QuestionRequest.builder()
+                .question(question)
+                .build();
+    }
+}
