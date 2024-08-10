e3fd8ff6bd2194122221b9fc797febb16d006008
han16935 <han1693516@gmail.com>
2024-08-10 17:07:13 +0900

    Add QuestionController

--- src/main/java/com/aiia/gpt_be/QuestionController.java
+++ src/main/java/com/aiia/gpt_be/QuestionController.java
@@ -1,4 +1,34 @@
 package com.aiia.gpt_be;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequiredArgsConstructor
 public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @GetMapping("/")
+    public String questionPage(){
+        return "homePage";
+    }
+
+    @PostMapping("/ask")
+    public QuestionReply answer(@Valid @RequestBody QuestionRequest questionRequest) {
+        return questionService.answer(questionRequest.to(), LocalDateTime.now());
+    }
+
+//    @PostMapping("/ask")
+//    public String answer(@Valid @RequestBody QuestionRequest questionRequest, Model model) {
+//        QuestionReply reply = questionService.answer(questionRequest.to(), LocalDateTime.now());
+//        model.addAttribute("reply", reply);
+//        return "redirect:/";
+//    }
 }
