bd8ef17476a517dccd37a888c3b9192d07d8ada9
han16935 <han1693516@gmail.com>
2024-08-21 17:00:02 +0900

    feature/Make passwordEncoder

---
+++ src/main/java/com/aiia/gpt_be/security/PasswordEncoder.java
@@ -0,0 +1,42 @@
+package com.aiia.gpt_be.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.MessageDigest;
+
+@Component
+public class PasswordEncoder {
+
+    @Value("${security.salt}")
+    private String salt;
+
+    public String encrypt(String password) {
+        try {
+
+            // 1. SHA256 알고리즘 객체 생성
+            MessageDigest md;
+            md = MessageDigest.getInstance("SHA-256");
+
+            // 2. password와 salt 합친 문자열에 SHA-256 알고리즘 적용
+            md.update((password+salt).getBytes());
+            byte[] pwdsalt = md.digest();
+
+            // 3. Encrypt된 결과를 문자열로 변환
+            StringBuilder sb = new StringBuilder();
+            for(byte b : pwdsalt) {
+                sb.append(String.format("%02x", b));
+            }
+
+            return sb.substring(0, 15);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Security error occurs";
+        }
+    }
+
+    public boolean checkSamePassword(String encryptedPassword, String requestedPassword) {
+        return encryptedPassword.equals(encrypt(requestedPassword));
+    }
+}
