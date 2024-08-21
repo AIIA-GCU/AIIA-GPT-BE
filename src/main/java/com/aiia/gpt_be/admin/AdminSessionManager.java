98daf542507adc113d7c8151911ef4fad69931ff
han16935 <han1693516@gmail.com>
2024-08-21 14:24:32 +0900

    feature/Make AdminSessionManager to manage session

---
+++ src/main/java/com/aiia/gpt_be/admin/AdminSessionManager.java
@@ -0,0 +1,27 @@
+package com.aiia.gpt_be.admin;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class AdminSessionManager {
+
+    private static final String ADMIN_SESSION_KEY="ADMIN";
+
+
+    public void saveAdminInSession(Admin admin, HttpServletRequest servletRequest) {
+        servletRequest.getSession().setAttribute(ADMIN_SESSION_KEY, admin);
+    }
+
+    public Optional<Admin> extractAdminFromSession(HttpServletRequest servletRequest) {
+        return Optional.ofNullable( (Admin) servletRequest.getSession().getAttribute(ADMIN_SESSION_KEY));
+    }
+
+    public boolean checkAdminLogin(HttpServletRequest servletRequest){
+        return extractAdminFromSession(servletRequest).isPresent();
+    }
+}
