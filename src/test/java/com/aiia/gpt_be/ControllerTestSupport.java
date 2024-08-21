ac13c7cf09a178e64a65374d1b04f3dcca911dcc
han16935 <han1693516@gmail.com>
2024-08-21 21:51:12 +0900

    test/Add test properties and inject dependencies by mocking at controller test env

--- src/test/java/com/aiia/gpt_be/ControllerTestSupport.java
+++ src/test/java/com/aiia/gpt_be/ControllerTestSupport.java
@@ -1,11 +1,16 @@
 package com.aiia.gpt_be;
 
-import com.aiia.gpt_be.admin.AdminController;
+import com.aiia.gpt_be.admin.AdminSessionManager;
+import com.aiia.gpt_be.admin.controller.AdminController;
+import com.aiia.gpt_be.admin.service.AdminService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+@ActiveProfiles("test")
 @WebMvcTest(controllers = {
         AdminController.class
 })
@@ -15,4 +20,10 @@ public abstract class ControllerTestSupport {
 
     @Autowired
     protected ObjectMapper objectMapper;
+
+    @MockBean
+    protected AdminService adminService;
+
+    @MockBean
+    protected AdminSessionManager adminSessionManager;
 }
