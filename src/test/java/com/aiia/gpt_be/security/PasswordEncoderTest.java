06c1adbf064b7bcc1fbe701977aa7179cb7bd020
han16935 <han1693516@gmail.com>
2024-08-21 17:01:16 +0900

    test/test encrypt & checkSamePassword method

---
+++ src/test/java/com/aiia/gpt_be/security/PasswordEncoderTest.java
@@ -0,0 +1,58 @@
+package com.aiia.gpt_be.security;
+
+import com.aiia.gpt_be.IntegrationTestSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordEncoderTest extends IntegrationTestSupport {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @DisplayName("문자열을 암호화할 수 있다.")
+    @Test
+    void encrypt_green() {
+        // given
+        String password = "password";
+
+        // when
+        String result = passwordEncoder.encrypt(password);
+
+        // then
+        assertThat(result).isNotEqualTo(password).hasSize(15);
+    }
+
+    @DisplayName("두 비밀번호가 같은 지 확인할 수 있다.")
+    @Test
+    void checkSamePassword_green() {
+        // given
+        String originalPassword = "password";
+        String encryptedPassword = passwordEncoder.encrypt(originalPassword);
+        String requestedPassword = "password";
+
+        // when
+        boolean result = passwordEncoder.checkSamePassword(encryptedPassword, requestedPassword);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("두 비밀번호가 다른 지 확인할 수 있다.")
+    @Test
+    void checkSamePassword_wrongPassword() {
+        // given
+        String originalPassword = "password";
+        String encryptedPassword = passwordEncoder.encrypt(originalPassword);
+        String wrongPassword = "wrong";
+
+        // when
+        boolean result = passwordEncoder.checkSamePassword(encryptedPassword, wrongPassword);
+
+        // then
+        assertThat(result).isFalse();
+    }
+}
