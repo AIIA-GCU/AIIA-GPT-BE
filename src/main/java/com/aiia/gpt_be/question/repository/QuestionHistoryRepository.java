a2d84f7fda47917af39f0d5ebd84d25f89a5403c
han16935 <han1693516@gmail.com>
2024-08-22 18:59:26 +0900

    restructure class in question directory

--- src/main/java/com/aiia/gpt_be/question/repository/QuestionHistoryRepository.java
+++ src/main/java/com/aiia/gpt_be/question/repository/QuestionHistoryRepository.java
@@ -1,6 +1,7 @@
-package com.aiia.gpt_be.question;
+package com.aiia.gpt_be.question.repository;
 
 
+import com.aiia.gpt_be.question.QuestionHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionHistoryRepository extends JpaRepository<QuestionHistory, Long> {
