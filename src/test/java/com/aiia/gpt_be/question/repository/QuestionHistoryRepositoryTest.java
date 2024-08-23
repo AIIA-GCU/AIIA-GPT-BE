36e0c5ab262e5f7216f92612f3d6981852a0a00c
han16935 <han1693516@gmail.com>
2024-08-23 16:05:59 +0900

    test/test getAllHistories() method in questionHistoryRepository

---
+++ src/test/java/com/aiia/gpt_be/question/repository/QuestionHistoryRepositoryTest.java
@@ -0,0 +1,86 @@
+package com.aiia.gpt_be.question.repository;
+
+import com.aiia.gpt_be.IntegrationTestSupport;
+import com.aiia.gpt_be.question.QuestionHistory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionHistoryRepositoryTest extends IntegrationTestSupport {
+
+    @Autowired
+    QuestionHistoryRepository questionHistoryRepository;
+
+    @AfterEach
+    void tearDown() {
+        questionHistoryRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("질문 기록을 페이징해 가져올 수 있다.")
+    @Test
+    void getAllHistories_green() {
+        // given
+        Pageable pageable = PageRequest.of(0, 2);
+        LocalDateTime now = LocalDateTime.of(2024, 8, 23, 0, 0, 0);
+
+        QuestionHistory q1 = QuestionHistory.of("question1", "answer1", now);
+        QuestionHistory q2 = QuestionHistory.of("question2", "answer2", now.plusDays(1));
+        QuestionHistory q3 = QuestionHistory.of("question3", "answer3", now.plusDays(2));
+        questionHistoryRepository.saveAll(List.of(q1, q2, q3));
+
+        // when
+        Page<QuestionHistory> pagedResults = questionHistoryRepository.getAllHistories(pageable);
+
+        // then
+        long totalCount = pagedResults.getTotalElements();
+        List<QuestionHistory> result = pagedResults.getContent();
+
+        assertThat(totalCount).isEqualTo(3);
+        assertThat(result).hasSize(2)
+                .extracting("question", "talkedTime")
+                .containsExactlyInAnyOrder(
+                        tuple("question1", now),
+                        tuple("question2", now.plusDays(1))
+                );
+    }
+
+    @DisplayName("질문 기록의 양이 페이지 사이즈보다 작을 경우, 전체 데이터를 가져올 수 있다.")
+    @Test
+    void getAllHistories_withSmallerHistorySize(){
+        // given
+        Pageable pageable = PageRequest.of(0, 5);
+        LocalDateTime now = LocalDateTime.of(2024, 8, 23, 0, 0, 0);
+
+        QuestionHistory q1 = QuestionHistory.of("question1", "answer1", now);
+        QuestionHistory q2 = QuestionHistory.of("question2", "answer2", now.plusDays(1));
+        QuestionHistory q3 = QuestionHistory.of("question3", "answer3", now.plusDays(2));
+        questionHistoryRepository.saveAll(List.of(q1, q2, q3));
+
+        // when
+        Page<QuestionHistory> pagedResults = questionHistoryRepository.getAllHistories(pageable);
+
+        // then
+        long totalCount = pagedResults.getTotalElements();
+        List<QuestionHistory> result = pagedResults.getContent();
+
+        assertThat(totalCount).isEqualTo(3);
+        assertThat(result).hasSize(3)
+                .extracting("question", "talkedTime")
+                .containsExactlyInAnyOrder(
+                        tuple("question1", now),
+                        tuple("question2", now.plusDays(1)),
+                        tuple("question3", now.plusDays(2))
+                );
+    }
+}
