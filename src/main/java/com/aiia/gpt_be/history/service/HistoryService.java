bb7cf5a391f6dc53eec4c2ebe2ec666babf6a751
han16935 <han1693516@gmail.com>
2024-08-23 18:39:22 +0900

    feature/Make getAllHistories()

--- src/main/java/com/aiia/gpt_be/history/service/HistoryService.java
+++ src/main/java/com/aiia/gpt_be/history/service/HistoryService.java
@@ -2,12 +2,12 @@ package com.aiia.gpt_be.history.service;
 
 import com.aiia.gpt_be.history.dto.HistoryMetaInfo;
 import com.aiia.gpt_be.history.repository.HistoryRepository;
+import com.aiia.gpt_be.question.QuestionHistory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +16,8 @@ public class HistoryService {
 
     private final HistoryRepository historyRepository;
 
-    public Page<HistoryMetaInfo> getAllHistories(Pageable pageable){
-        return null;
+    public Page<HistoryMetaInfo> getAllHistories(Pageable pageable) {
+        return historyRepository.getAllHistories(pageable)
+                .map(QuestionHistory::toHistoryMetaInfo);
     }
 }
