4fb84e424dca303516c7334f1ee09fc83a11d367
han16935 <han1693516@gmail.com>
2024-08-23 16:45:37 +0900

    refactor/delete unused import statement

--- src/test/java/com/aiia/gpt_be/admin/repository/AdminRepositoryTest.java
+++ src/test/java/com/aiia/gpt_be/admin/repository/AdminRepositoryTest.java
@@ -11,8 +11,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.tuple;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AdminRepositoryTest extends IntegrationTestSupport {
 
