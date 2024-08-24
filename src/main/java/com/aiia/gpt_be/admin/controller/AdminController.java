50572725802275e48583d4afb713fb88780304de
han16935 <han1693516@gmail.com>
2024-08-24 17:39:33 +0900

    feature/Add histories to mainPage

--- src/main/java/com/aiia/gpt_be/admin/controller/AdminController.java
+++ src/main/java/com/aiia/gpt_be/admin/controller/AdminController.java
@@ -5,11 +5,16 @@ import com.aiia.gpt_be.admin.dto.AdminLoginRequest;
 import com.aiia.gpt_be.admin.service.AdminService;
 import com.aiia.gpt_be.admin.AdminSessionManager;
 import com.aiia.gpt_be.admin.dto.AdminJoinRequest;
+import com.aiia.gpt_be.history.dto.HistoryMetaInfo;
 import com.aiia.gpt_be.history.service.HistoryService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -19,11 +24,13 @@ public class AdminController {
 
     private final AdminService adminService;
 
+    private final HistoryService historyService;
+
     private final AdminSessionManager adminSessionManager;
 
     @GetMapping(value = {"", "/", "/login"})
-    public String loginPage(HttpServletRequest request){
-        if(adminSessionManager.checkAdminLogin(request)) {
+    public String loginPage(HttpServletRequest request) {
+        if (adminSessionManager.checkAdminLogin(request)) {
             return "redirect:/admin/main";
         }
         return "admin/login";
@@ -38,17 +45,24 @@ public class AdminController {
     }
 
     @GetMapping("/main")
-    public String mainPage(HttpServletRequest request) {
-        if(adminSessionManager.checkAdminLogin(request)) {
-            return "admin/main";
-        }
-        return "error/400";
+    public String mainPage(@PageableDefault(page = 0, size = 10) Pageable pageable, Model model) {
+        Page<HistoryMetaInfo> histories = historyService.getAllHistories(pageable);
+
+        int blockLimit = 3;
+        int startPage = (((int) Math.ceil(((double) pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1;
+        int endPage = Math.min((startPage + blockLimit - 1), histories.getTotalPages());
+
+        model.addAttribute("histories", histories);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        return "admin/main";
     }
 
 
     @PostMapping("/join")
     public String join(@Valid @ModelAttribute AdminJoinRequest joinRequest) {
-          adminService.join(joinRequest);
-          return "redirect:/";
+        adminService.join(joinRequest);
+        return "redirect:/";
     }
 }
