77cbd8d206f92d2aad929908d90dfa8ba072ddb7
han16935 <han1693516@gmail.com>
2024-08-25 19:11:04 +0900

    refactor/set HTTP Status & get errorMessage made by redirectAttributes

--- src/main/java/com/aiia/gpt_be/error/ErrorPageController.java
+++ src/main/java/com/aiia/gpt_be/error/ErrorPageController.java
@@ -2,7 +2,9 @@ package com.aiia.gpt_be.error;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static jakarta.servlet.http.HttpServletResponse.*;
@@ -11,21 +13,32 @@ import static jakarta.servlet.http.HttpServletResponse.*;
 @RequestMapping("/error")
 public class ErrorPageController {
 
+    private static final String ERROR_MESSAGE = "errorMessage";
+
     @GetMapping("/400")
-    public String badRequestErrorPage(HttpServletResponse response){
+    public String badRequestErrorPage(@ModelAttribute(ERROR_MESSAGE) String errorMessage,
+                                      HttpServletResponse response,
+                                      Model model){
         response.setStatus(SC_BAD_REQUEST);
+        model.addAttribute(ERROR_MESSAGE, errorMessage);
         return "/error/400";
     }
 
     @GetMapping("/401")
-    public String unAuthorizedErrorPage(HttpServletResponse response){
+    public String unAuthorizedErrorPage(@ModelAttribute(ERROR_MESSAGE) String errorMessage,
+                                        HttpServletResponse response,
+                                        Model model){
         response.setStatus(SC_UNAUTHORIZED);
+        model.addAttribute(ERROR_MESSAGE, errorMessage);
         return "/error/401";
     }
 
     @GetMapping("/500")
-    public String internalServerErrorPage(HttpServletResponse response){
+    public String internalServerErrorPage(@ModelAttribute(ERROR_MESSAGE) String errorMessage,
+                                          HttpServletResponse response,
+                                          Model model){
         response.setStatus(SC_INTERNAL_SERVER_ERROR);
+        model.addAttribute(ERROR_MESSAGE, errorMessage);
         return "/error/500";
     }
 }
