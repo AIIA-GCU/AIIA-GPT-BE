da9a8cf371a30168f5ee9f86f61bfbfa1a3be889
han16935 <han1693516@gmail.com>
2024-08-25 19:35:49 +0900

    secure/Add clientIpHandler to check client's IP and add more logic when unauthorized access happens

--- src/main/java/com/aiia/gpt_be/security/AdminLoginCheckInterceptor.java
+++ src/main/java/com/aiia/gpt_be/security/AdminLoginCheckInterceptor.java
@@ -4,14 +4,31 @@ import com.aiia.gpt_be.admin.AdminSessionManager;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import static jakarta.servlet.http.HttpServletResponse.*;
+
+@Slf4j
 @AllArgsConstructor
 public class AdminLoginCheckInterceptor implements HandlerInterceptor {
     private AdminSessionManager adminSessionManager;
+    private ClientIpHandler clientIpHandler;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        return adminSessionManager.checkAdminLogin(request);
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        boolean doesAdminNotLoggedIn = adminSessionManager.checkAdminNotLogin(request);
+        if(doesAdminNotLoggedIn) {
+            String clientIp = clientIpHandler.getClientIpFrom(request);
+
+            log.warn("Unauthorized access !!! Client IP = {}, Detected time = {}", clientIp, LocalDateTime.now());
+
+            response.sendRedirect("/error/401");
+            return false;
+        }
+        return true;
     }
 }
