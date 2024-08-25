caf3ea693b5b6e7885ce66a5d8254550dd5e5476
han16935 <han1693516@gmail.com>
2024-08-25 19:11:37 +0900

    test/Add more test logic with flashAttr & inspect model

--- src/test/java/com/aiia/gpt_be/error/ErrorPageControllerTest.java
+++ src/test/java/com/aiia/gpt_be/error/ErrorPageControllerTest.java
@@ -7,10 +7,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class ErrorPageControllerTest extends ControllerTestSupport {
+
+    private static final String ERROR_MESSAGE = "errorMessage";
 
     @DisplayName("400 페이지로 갈 수 있다.")
     @Test
@@ -18,10 +19,12 @@ class ErrorPageControllerTest extends ControllerTestSupport {
         // given
 
         // when // then
-        mockMvc.perform(get("/error/400"))
+        mockMvc.perform(get("/error/400")
+                        .flashAttr(ERROR_MESSAGE, "message"))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
-                .andExpect(view().name("/error/400"));
+                .andExpect(view().name("/error/400"))
+                .andExpect(model().attribute(ERROR_MESSAGE, "message"));
     }
 
     @DisplayName("401 페이지로 갈 수 있다.")
@@ -30,10 +33,12 @@ class ErrorPageControllerTest extends ControllerTestSupport {
         // given
 
         // when // then
-        mockMvc.perform(get("/error/401"))
+        mockMvc.perform(get("/error/401")
+                        .flashAttr(ERROR_MESSAGE, "message"))
                 .andDo(print())
                 .andExpect(status().isUnauthorized())
-                .andExpect(view().name("/error/401"));
+                .andExpect(view().name("/error/401"))
+                .andExpect(model().attribute(ERROR_MESSAGE, "message"));
     }
 
     @DisplayName("500 페이지로 갈 수 있다.")
@@ -42,9 +47,11 @@ class ErrorPageControllerTest extends ControllerTestSupport {
         // given
 
         // when // then
-        mockMvc.perform(get("/error/500"))
+        mockMvc.perform(get("/error/500")
+                        .flashAttr(ERROR_MESSAGE, "message"))
                 .andDo(print())
                 .andExpect(status().isInternalServerError())
-                .andExpect(view().name("/error/500"));
+                .andExpect(view().name("/error/500"))
+                .andExpect(model().attribute(ERROR_MESSAGE, "message"));
     }
 }
