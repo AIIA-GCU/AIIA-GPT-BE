664294f0db46d1a7cd477aa1f5b1182ff6c1cb89
han16935 <han1693516@gmail.com>
2024-08-26 17:58:08 +0900

    fix/add validation logic if totalPageCount == 0

--- src/main/java/com/aiia/gpt_be/history/vo/PageIndex.java
+++ src/main/java/com/aiia/gpt_be/history/vo/PageIndex.java
@@ -14,18 +14,23 @@ public class PageIndex {
         int currentPageNumber = pageable.getPageNumber();
         int totalPageCount = pages.getTotalPages();
 
-        this.startPageNumber = getStartPageNumber(currentPageNumber, blockLimit);
+        this.startPageNumber = getStartPageNumber(currentPageNumber, totalPageCount, blockLimit);
         this.endPageNumber = getEndPageNumber(currentPageNumber, totalPageCount, blockLimit);
     }
 
-    private int getStartPageNumber(int currentPageNumber, int blockLimit){
-        if(doesInitialPage(currentPageNumber)) return 0;
+    private int getStartPageNumber(int currentPageNumber, int totalPageCount, int blockLimit){
+        if (doesTotalPageEmpty(totalPageCount) || doesInitialPage(currentPageNumber)) return 0;
         else return currentPageNumber - blockLimit;
     }
 
     private int getEndPageNumber(int currentPageNumber, int totalPageCount, int blockLimit) {
+        if (doesTotalPageEmpty(totalPageCount)) return 0;
         if (doesLastPage(currentPageNumber, totalPageCount)) return totalPageCount - blockLimit;
         else return currentPageNumber + blockLimit;
+    }
+
+    private boolean doesTotalPageEmpty(int totalPageCount) {
+        return totalPageCount == 0;
     }
 
     private boolean doesInitialPage(int currentPageNumber) {
