ff91c5aa2633464917717e0b4adaf68ed7c1af63
han16935 <han1693516@gmail.com>
2024-08-26 16:27:22 +0900

    test/Add getHistory test case & add HistoryControlelr to ControllerTestSupport

--- src/test/java/com/aiia/gpt_be/ControllerTestSupport.java
+++ src/test/java/com/aiia/gpt_be/ControllerTestSupport.java
@@ -4,6 +4,7 @@ import com.aiia.gpt_be.admin.AdminSessionManager;
 import com.aiia.gpt_be.admin.controller.AdminController;
 import com.aiia.gpt_be.admin.service.AdminService;
 import com.aiia.gpt_be.error.ErrorPageController;
+import com.aiia.gpt_be.history.controller.HistoryController;
 import com.aiia.gpt_be.history.service.HistoryService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @ActiveProfiles("test")
 @WebMvcTest(controllers = {
         AdminController.class,
+        HistoryController.class,
         ErrorPageController.class
 })
 public abstract class ControllerTestSupport {
@@ -32,4 +34,7 @@ public abstract class ControllerTestSupport {
 
     @MockBean
     protected AdminSessionManager adminSessionManager;
+
+    protected static final String ADMIN_SESSION_KEY="ADMIN";
+    protected static final String ERROR_MESSAGE = "errorMessage";
 }
---
+++ src/test/java/com/aiia/gpt_be/history/controller/HistoryControllerTest.java
@@ -0,0 +1,63 @@
+package com.aiia.gpt_be.history.controller;
+
+import com.aiia.gpt_be.ControllerTestSupport;
+import com.aiia.gpt_be.admin.Admin;
+import com.aiia.gpt_be.history.dto.HistoryInfo;
+import com.aiia.gpt_be.history.dto.HistoryInfoRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class HistoryControllerTest extends ControllerTestSupport {
+
+
+    @DisplayName("사용자의 질문 기록을 가져올 수 있다.")
+    @Test
+    void getHistory_green() throws Exception {
+        // given
+        Admin admin = Admin.of("id", "password");
+        given(adminSessionManager.checkAdminNotLogin(any(HttpServletRequest.class)))
+                .willReturn(false);
+
+        given(historyService.getHistory(any(HistoryInfoRequest.class)))
+                .willReturn(HistoryInfo.of("q1", "a1", "time"));
+
+        // when // then
+        mockMvc.perform(get("/history/1")
+                        .sessionAttr(ADMIN_SESSION_KEY, admin))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("/history/view"))
+                .andExpect(model().attributeExists("history"));
+    }
+
+//    @DisplayName("질문 기록을 가져올 때, 잘못된 접근을 확인할 수 있다.")
+//    @CsvSource(value = {"0", "-1", "abcde", "ㄱㄴㄷㄹ", "!@#$%"})
+//    @ParameterizedTest(name = "{0}")
+//    void getHistory_invalidID(String invalidPath) throws Exception {
+//        // given
+//        Admin admin = Admin.of("id", "password");
+//        given(adminSessionManager.checkAdminNotLogin(any(HttpServletRequest.class)))
+//                .willReturn(false);
+//
+//        // when // then
+//        mockMvc.perform(get("/history/" + invalidPath)
+//                        .sessionAttr(ADMIN_SESSION_KEY, admin))
+//                .andDo(print())
+//                .andExpect(status().is3xxRedirection())
+//                .andExpect(view().name("redirect:/error/400"))
+//                .andExpect(flash().attribute(ERROR_MESSAGE, "잘못된 값입니다!"));
+//    }
+}
