e888dc453356f01e31c8fd30654f9a6f522363d3
han16935 <han1693516@gmail.com>
2024-08-26 09:58:54 +0900

    Add PageIndex VO to manage paging range

---
+++ src/main/java/com/aiia/gpt_be/history/vo/PageIndex.java
@@ -0,0 +1,38 @@
+package com.aiia.gpt_be.history.vo;
+
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+@Getter
+public class PageIndex {
+
+    private final int startPageNumber;
+    private final int endPageNumber;
+
+    public PageIndex(Page pages, Pageable pageable, int blockLimit) {
+        int currentPageNumber = pageable.getPageNumber();
+        int totalPageCount = pages.getTotalPages();
+
+        this.startPageNumber = getStartPageNumber(currentPageNumber, blockLimit);
+        this.endPageNumber = getEndPageNumber(currentPageNumber, totalPageCount, blockLimit);
+    }
+
+    private int getStartPageNumber(int currentPageNumber, int blockLimit){
+        if(doesInitialPage(currentPageNumber)) return 0;
+        else return currentPageNumber - blockLimit;
+    }
+
+    private int getEndPageNumber(int currentPageNumber, int totalPageCount, int blockLimit) {
+        if (doesLastPage(currentPageNumber, totalPageCount)) return totalPageCount - blockLimit;
+        else return currentPageNumber + blockLimit;
+    }
+
+    private boolean doesInitialPage(int currentPageNumber) {
+        return currentPageNumber == 0;
+    }
+
+    private boolean doesLastPage(int currentPageNumber, int totalPageCount) {
+        return currentPageNumber == totalPageCount - 1;
+    }
+}
