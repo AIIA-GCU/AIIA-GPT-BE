956bf27af5377f4455e38c7870ed8c99deec90b9
han16935 <han1693516@gmail.com>
2024-08-26 10:21:54 +0900

    feature/Add findById to HistoryRepository

--- src/main/java/com/aiia/gpt_be/history/repository/HistoryRepository.java
+++ src/main/java/com/aiia/gpt_be/history/repository/HistoryRepository.java
@@ -3,5 +3,8 @@ package com.aiia.gpt_be.history.repository;
 import com.aiia.gpt_be.question.QuestionHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HistoryRepository extends JpaRepository<QuestionHistory, Long>, HistoryRepositoryCustom {
+    Optional<QuestionHistory> findById(Long id);
 }
